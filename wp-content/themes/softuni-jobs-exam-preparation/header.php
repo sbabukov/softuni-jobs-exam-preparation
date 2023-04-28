cd3a94b568df2ce731ca97f80bcd5bb6bc94ddf6
sbabukov <s.babukov@gmail.com>
2023-04-28 11:55:42 +0300

    Initial commit of archive pages

--- wp-content/themes/softuni-jobs-exam-preparation/header.php
+++ wp-content/themes/softuni-jobs-exam-preparation/header.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Jobs</title>
-	<link rel="preconnect" href="https://fonts.gstatic.com">
-	<link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="<?php bloginfo('charset'); ?>" />
+    <link rel="profile" href="//gmpg.org/xfn/11" />
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
+    
+    <title><?php wp_title(); ?></title>
 </head>
 
 <!-- Викане на енкюнването на стиловете -->
@@ -15,5 +19,10 @@
 <body>
 	<div class="site-wrapper">
 		<header class="site-header">
-			<h1 class="site-title"><a href="#">Job Offers</a></h1>
+			<?php if ( is_home() ): ?>
+			<h1 class="site-title"><a href="<?php echo esc_url( get_home_url()); ?>">Job Offers</a></h1>
+			<?php else: ?>
+				<p class="site-title"><a href="<?php echo esc_url( get_home_url()); ?>">Job Offers</a></p>
+			<?php endif; ?>	
+			<!-- <h1 class="site-title"><a href="<?php echo esc_url( get_home_url()); ?>">Job Offers</a></h1> -->
 		</header>
